e921d776f9c74e3286868d995119acfb35ef2cc1
rabby783islam-jpg <rabby783islam@gmail.com>
2026-03-08 09:03:33 +0600

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T03:03:32.653Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T03:03:41.829Z
+Commit: Fix broken links in documentation
