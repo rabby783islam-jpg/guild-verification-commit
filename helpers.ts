abc66ebf0d5ca2e02bc9f0cd34e1f730eac75f57
rabby783islam-jpg <rabby783islam@gmail.com>
2026-03-08 09:04:58 +0600

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T03:04:36.484Z
-Commit: Add multi-language support
+Timestamp: 2026-03-08T03:05:06.003Z
+Commit: Resolve null pointer exception in user service
